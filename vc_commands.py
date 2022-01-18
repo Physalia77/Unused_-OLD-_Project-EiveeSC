8e3ecba6697dcfca25b87d534213cc6d4b99a2b5
Physalia77 <kevin.angelstrom@gmail.com>
2022-01-18 02:50:17 +0100

    added more stuff to the To-do-list.txt and cleaned it up to make it easier to focus. its now sorted by stuff that has to be done in each file

--- vc_commands.py
+++ vc_commands.py
@@ -6,36 +6,35 @@ Voice-Chat commands
 """
 
 
-class vc_c(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.last_member = None
+class main(commands.Cog):
+    def init(self, Bot):
+        pass
 
     # Joins VC
     @commands.command()
-    async def join(ctx):
+    async def join(self, ctx):
         if ctx.author.voice is None:
             await ctx.send(f"Please, may {ctx.author} join a voice channel for me to direct to!")
 
         voice_channel = ctx.author.voice.channel
-        if ctx.voice_commands is None:
+        if ctx.voice_client is None:
             await voice_channel.connect()
         else:
-            await ctx.voice_commands.move_to(voice_channel)
+            await ctx.voice_client.move_to(voice_channel)
 
     # The bot leave's
     @commands.command()
-    async def leave(ctx):
-        await ctx.voice_commands.disconnect()
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
 
     # The bot plays the music
     @commands.command()
-    async def play(ctx, url):
+    async def play(self, ctx, url):
         ctx.voice_commands.stop()
         FFMPEG_OPTIONS = {'before_option': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay-max 5',
                           'options': '-vn'}
         YDL_OPTIONS = {'format': "bestaudio"}
-        vc = ctx.voice_commands
+        vc = ctx.voice_client
 
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
@@ -45,16 +44,16 @@ class vc_c(commands.Cog):
 
     # Pause
     @commands.command()
-    async def pause(ctx):
-        await ctx.voice_commands.pause()
+    async def pause(self, ctx):
+        await ctx.voice_client.pause()
         await ctx.send("It has been done!")
 
     # Resume
     @commands.command()
-    async def resume(ctx):
-        await ctx.voice_commands.resume()
+    async def resume(self, ctx):
+        await ctx.voice_client.resume()
         await ctx.send("It has been done!")
 
 
-def setup(bot: commands.Bot):
-    bot.add_cog(vc_c(bot))
+def setup(bot):
+    bot.add_cog(main(bot))
